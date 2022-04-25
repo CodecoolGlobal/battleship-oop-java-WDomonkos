b73e46c807c9a332f59448fd4f666517ca7a4630
reka <debreka@gmail.com>
2022-04-25 10:52:30 +0200

    add ship length to ship type enum

--- src/battleship/ShipType.java
+++ src/battleship/ShipType.java
@@ -1,9 +1,37 @@
 package battleship;
+public class ShipType{
 
-public enum ShipType {
-    CARRIER,
-    CRUISER,
-    BATTLESHIP,
-    SUBMARINE,
-    DESTROYER
+    private final ShipLength ship;
+
+    public ShipType (ShipLength ship){
+        this.ship = ship;
+    }
+
+    public int getShipLength(){
+        switch (this.ship){
+            case CARRIER:
+                return 2;
+            case CRUISER:
+                return 3;
+            case BATTLESHIP:
+                return 4;
+            case SUBMARINE:
+                return 5;
+            default:
+                return 6;
+        }
+    }
+
+    public enum ShipLength {
+        CARRIER,
+        CRUISER,
+        BATTLESHIP,
+        SUBMARINE,
+        DESTROYER
+    }
+
+
+
 }
+
+
