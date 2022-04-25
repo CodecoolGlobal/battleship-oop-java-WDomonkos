a72d15d7d07e5c2b51dff45c9e886bb21a8c4872
reka <debreka@gmail.com>
2022-04-25 12:11:45 +0200

    construct ship class

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -3,6 +3,20 @@ package battleship;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Ship {
-    List<Square> positions = new ArrayList<>();
+public class Ship {
+
+    private ArrayList<Square> position = new ArrayList<>();
+    private final Board board;
+    private final ShipType type;
+
+
+    public Ship(Board board, ShipType type){
+           this.board = board;
+           this.type = type;
+    }
+
+    public void addPositions(Square coordinate){
+           position.add(coordinate);
+    }
+
 }
