15412861c9aa7b1b79388f6d544ac10cc6940a09
WDomonkos <wdomi14@gmail.com>
2022-04-28 14:13:00 +0200

    emoticons instead of char

--- src/battleship/SquareStatus.java
+++ src/battleship/SquareStatus.java
@@ -1,19 +1,19 @@
 package battleship;
 
 public enum SquareStatus {
-        EMPTY('.'),
-        NEIGHBOUR('.'),
-        SHIP('.'),
-        HIT('X'),
-        MISSED('O');
-    private final char symbol;
+    EMPTY("\uD83C\uDF0A"),
+    NEIGHBOUR("\uD83C\uDF0A"),
+    SHIP("\uD83C\uDF0A"),
+    HIT("\uD83D\uDCA5"),
+    MISSED("\uD83D\uDCA4");
+    private final String symbol;
 
-    SquareStatus(char symbol) {
+    SquareStatus(String symbol) {
         this.symbol = symbol;
     }
-    public char getCharacter() {
+
+    public String getCharacter() {
         return symbol;
     }
-
 }
 
